099fbc2afdeddd74f9b1c035a402feb0e11da993
sarthakshukla1856-debug <sarthakshukla1856@gmail.com>
2025-09-17 10:57:28 +0530

    COMMIT

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlops_project_1",
+    version="0.1",
+    author="Sarthak",
+    packages=find_packages(),
+    install_requires=[
+    "numpy",
+    "pandas",
+    "scikit-learn",
+    "google-cloud-storage",
+    "pyyaml",
+    "imbalanced-learn",
+    "lightgbm",
+    "mlflow"
+],
+)
